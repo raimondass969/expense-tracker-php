6aeecbe7ac4da695bfc7e16cf3870c5f1e81834b
Your Name <you@example.com>
2026-08-26 18:29:28 +0300

    feat: add Category and Transactions models

---
+++ src/Models/Category.php
@@ -0,0 +1,40 @@
+<?php
+
+class Category
+{
+
+    private $pdo;
+    public function __construct(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    public function addCategory($userId, $name)
+    {
+
+        try {
+            $stmt = $this->pdo->prepare("INSERT INTO categories (user_id,NAME) values (:user_id, :NAME) ");
+            $stmt->execute([
+                'user_id' => $userId,
+                'NAME' => $name,
+            ]);
+            return true;
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    public function getCategoriesForUser($userId)
+    {
+
+        try {
+            $stmt = $this->pdo->prepare("SELECT * FROM categories WHERE user_id = :user_id");
+            $stmt->execute([
+                'user_id' => $userId,
+            ]);
+            return  $stmt->fetchAll();
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+}
